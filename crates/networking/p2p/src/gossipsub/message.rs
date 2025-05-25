a4eb1dc9b910d6950f6da385bf5a81b9022583d5
Kayden-ML <kaydenmoroz82@gmail.com>
2025-05-24 20:56:11 -0600

    feat: implement gossip topic sync_committee_contribution_and_proof

--- crates/networking/p2p/src/gossipsub/message.rs
+++ crates/networking/p2p/src/gossipsub/message.rs
@@ -5,7 +5,9 @@ use ream_consensus::{
     electra::beacon_block::SignedBeaconBlock, sync_committee::SyncCommittee,
 };
 use ream_network_spec::networks::network_spec;
-use ream_validator::aggregate_and_proof::AggregateAndProof;
+use ream_validator::{
+    aggregate_and_proof::AggregateAndProof, contribution_and_proof::ContributionAndProof,
+};
 use ssz::Decode;
 
 use super::{
@@ -22,6 +24,7 @@ pub enum GossipsubMessage {
     BeaconAttestation(Box<Attestation>),
     SyncCommittee(Box<SyncCommittee>),
     BlsToExecutionChange(Box<BLSToExecutionChange>),
+    SyncCommitteeContributionAndProof(Box<ContributionAndProof>),
 }
 
 impl GossipsubMessage {
@@ -50,6 +53,11 @@ impl GossipsubMessage {
             GossipTopicKind::BlsToExecutionChange => Ok(Self::BlsToExecutionChange(Box::new(
                 BLSToExecutionChange::from_ssz_bytes(data)?,
             ))),
+            GossipTopicKind::SyncCommitteeContributionAndProof => {
+                Ok(Self::SyncCommitteeContributionAndProof(Box::new(
+                    ContributionAndProof::from_ssz_bytes(data)?,
+                )))
+            }
             GossipTopicKind::AttesterSlashing => Ok(Self::AttesterSlashing(Box::new(
                 AttesterSlashing::from_ssz_bytes(data)?,
             ))),
